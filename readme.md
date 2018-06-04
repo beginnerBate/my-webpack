4366acff40ee60cb029c9ee7fb648c5428ddbc4f
liuyeqian <liuyeq2015@163.com>
2018-06-04 11:49:49 +0800

    Output Management

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
-# Getting Started ---branch mywebpack01
+# Getting Started    ---branch mywebpack01
+# Asset Management   ---branch mywebpack02
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import _ from 'lodash'
+import './style.css'
+import Icon from './icon.png'
 function component () {
   var element = document.createElement('div');
   
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello','webpack'], '');
+  element.classList.add('hello')
+
+  // Add the image to our existing div.
+  var myIcon = new Image()
+  myIcon.src = Icon
+  myIcon.height =50
+  myIcon.width = 50
+
+  element.appendChild(myIcon)
   return element; 
 }
 
